435eb60bde44b4eaf943e3736448398f8ed680b5
etnosd01 <snhssn00169@naver.com>
2023-12-21 00:59:10 +0900

    set up goal db

---
+++ app/src/main/java/com/example/showmethemoneyproject/AmountModel.kt
@@ -0,0 +1,14 @@
+package com.example.showmethemoneyproject
+
+data class AmountModel (
+    val balance : Int = 0,
+    val food : Int = 0,
+    val car : Int = 0,
+    val edu : Int = 0,
+    val home : Int = 0,
+    val saving : Int = 0,
+    val Hobby : Int = 0,
+    val Cafe : Int = 0,
+    val Account : Int = 0,
+    val etc : Int = 0
+)
